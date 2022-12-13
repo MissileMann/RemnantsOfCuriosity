b368b1d35b243c98d67c970183ea8e2487488512
MissileMann <misael.escoto043@gmail.com>
2022-12-13 00:52:50 -0600

    Initial Class Added

---
+++ src/main/java/io/github/missilemann/remnantsofcuriosity/RemnantsOfCuriosities.java
@@ -0,0 +1,8 @@
+package io.github.missilemann.remnantsofcuriosity;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(RemnantsOfCuriosities.MODID)
+public class RemnantsOfCuriosities {
+    public static final String MODID = "remnantsofcuriosities";
+}
